3c5c00c2b28037fd6488dc752fa589c8348073f1
Mohammad Butt <mbutt@student.42.us.org>
2020-02-14 13:36:40 -0800

    Fixed a small issue in epur_str

--- day01/ex05/epur_str.php
+++ day01/ex05/epur_str.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php
-    $i = 0;
     if($argc != 2)
         return(0);
+    $i = 0;
     $str_array = preg_split('/\s+/', $argv[1], -1, PREG_SPLIT_NO_EMPTY);
     $final_str = "";
     $word_count = count($str_array);
@@ -10,5 +10,6 @@
         $final_str = $final_str.$str_array[$i++];
     while($i < $word_count)
         $final_str = $final_str." ".$str_array[$i++];
-    echo($final_str)."\n";
+    if($word_count > 0)
+        echo($final_str)."\n";
 ?>
