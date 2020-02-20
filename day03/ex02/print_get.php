1e6bb9ac8ee6b80fb6c8488b83a6c1cfb9ff607a
Mohammad Butt <maniqrb@gmail.com>
2020-02-19 21:55:44 -0800

    Cleaned up files and finished ex00, ex01, and ex02

---
+++ day03/ex02/print_get.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    References:https:
+    stackoverflow.com/questions/2160382/how-do-i-grab-all-parameters-from-a-url-and-print-it-out-in-php
+
+*/
+    
+    foreach($_GET as $key => $value)
+	echo($key).": ".$value."\n";
+?>
