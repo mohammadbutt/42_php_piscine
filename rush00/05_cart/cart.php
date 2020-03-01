5ccc60c44da02acc7a849131562c1fef90662e50
Mohammad Butt <mbutt@student.42.us.org>
2020-02-29 21:59:46 -0800

    Finished shopping cart

--- rush00/05_cart/cart.php
+++ rush00/05_cart/cart.php
@@ -4,6 +4,7 @@
     https://stackoverflow.com/questions/722379/can-html-be-embedded-inside-php-if-statement/38391978#38391978
     https://stackoverflow.com/questions/5673269/what-is-the-advantage-of-using-heredoc-in-php/5673478#5673478
     https://andy-carter.com/blog/what-are-php-heredoc-nowdoc
+    https://stackoverflow.com/questions/13882656/html-form-in-php-while-loop
 
     Notes:  1. Use heredoc syntax to embed html code inside php.
             2. seperate html file is created just for layout.
@@ -15,15 +16,17 @@
     $current_user = $_SESSION["logged_in_user"];
 
 ?>
+
 <!DOCTYPE html>
 <!--
     References:
     https://www.w3schools.com/html/html_form_input_types.asp
+    https://www.w3schools.com/html/html_tables.asp
 -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Cart</title>
+    <title>index.html</title>
 </head>
 <style>
     body
@@ -33,7 +36,6 @@
     h1
     {
         text-align: center;
-        
     }
     .user_placeholder
     {
@@ -66,15 +68,63 @@
     {
         text-align: center;
     }
+    .remove_button
+    {
+        font-size: 15px;
+        border-radius: 15px;
+    }
+    .remove_button:hover
+    {
+        background: rgb(255, 36, 73);
+    }
 </style>
 <body>
     <h1>42 E-site</h1>
 
     <p class="user_placeholder">
         <?php
-        echo($current_user);
+            echo($current_user);
         ?>
         <span class="cart">Shopping cart</span>
     </p>
+    <table style="width: 100%">
+        <tr>
+            <th>Item Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Change quantity</th>
+        </tr><br>
+        <?php
+            $i = 0;
+            $file_path = $current_user.".txt";
+            $file_array = unserialize(file_get_contents($file_path));
+            $array_count = count($file_array);
+            while($i < $array_count)
+            {
+                $item = $file_array[$i]["item"];
+                $quantity = $file_array[$i]["quantity"];
+                $price = $quantity * $file_array[$i]["price"];
+                $total_price = $price + $total_price;
+        ?>
+        <tr>
+            <th><?php echo($item); ?></th>
+            <th><?php echo($quantity); ?></th>
+            <th><?php echo('$ '.number_format($price)); ?></th>
+            <form action="#" method="POST">
+                <th>
+                    <button class="remove_button" type="submit" name="item" value="<?php echo($item); ?>">Remove Item</button>
+                </th>
+            </form>
+        </tr>
+        <?php
+            $i++;
+            }
+        ?>
+        <tr>
+            <th style="text-align: left">Total: <?php echo('$ '.number_format($total_price)); ?></th>
+        </tr>
+        <!-- Above done -->
+    </table>
 </body>
 </html>
+
