0572159ffeeeb4441412d41bf61039088b2a12e1
Mohammad Butt <mbutt@student.42.us.org>
2020-02-29 16:49:08 -0800

    Added shopping cart functionality

--- rush00/index.php
+++ rush00/index.php
@@ -14,26 +14,70 @@ function get_whoami()
 
 function get_item_price()
 {
-
+    $item_name = $_POST["name"];
+    $price = 0;
+    if(strcmp($item_name, "laptop_acer") == 0)
+        $price = 1199.99;
+    else if(strcmp($item_name, "laptop_msi") == 0)
+        $price = 949.99;
+    else if(strcmp($item_name, "mobile_iphone") == 0)
+        $price = 599.00;
+    else if(strcmp($item_name, "mobile_moto") == 0)
+        $price = 299.99;
+    else if(strcmp($item_name, "tablet_galaxy") == 0)
+        $price = 299.99;
+    else if(strcmp($item_name, "tablet_ipad") == 0)
+        $price = 329.99;
+    return($price);
 }
 
 function put_items_in_cart()
 {
     $price = get_item_price();
+    $logged_user = get_whoami();
+    $cart_file_path = "05_cart/".$logged_user.".txt";
+    $item_name = $_POST["item"];
+    if(file_exists($cart_file_path) == true)
+        $file_array = unserialize(file_get_contents($cart_file_path));
+    $i = 0;
+    $item_count = count($file_array);
+    if($item_count > 0)
+    {
+        while($i < $item_count)
+        {
+            if(strcmp($file_array[$i]["item"], $item_name) == 0)
+            {
+                $file_array[$i]["quantity"] = $file_array[$i]["quantity"] + 1;
+                file_put_contents($cart_file_path, serialize($file_array));
+                return(0);
+            }
+            $i++;
+        }
+    }
+    $file_array[] = array("item" => $item_name, "quantity" => 1);
+    file_put_contents($cart_file_path, serialize($file_array));
+    return(0);
 }
 
 /*
     main:
 */
-session_start();
-$logged_user = get_whoami;
-if(strcmp($logged_user, "") == 0)
-    header("Location: index.html");
-else
-{
-    put_items_in_cart();
-    header("Location: index.html");
-}
+
+    session_start();
+    $logged_user = get_whoami();
+
+    if(strcmp($logged_user, "") == 0)
+    {
+        header("refresh: 5; url=index.html");
+        echo("Please sign in to add items to cart<br>");
+        echo("Redirecting in 5 seconds<br>");
+    }
+    else
+    {
+        put_items_in_cart();
+        header("Location: index.html");
+    }
+
 /*
 if(strcmp($logged_user, "") != 0)
 {
